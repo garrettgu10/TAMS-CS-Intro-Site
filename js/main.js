7d98a9920e7091418f2a682e4ee1e7f405b72df7
Garrett Gu <garrettgu777@gmail.com>
2017-02-08 13:06:24 -0600

    made colors slightly lighter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function getRandomColor() {
-    var letters = '01234567';
+    var letters = '0123456789';
     var color = '#';
     for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 8)];
+        color += letters[Math.floor(Math.random() * 10)];
     }
     return color;
 }
